a819258c7933635392631951c08f3ba3428f4328
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-31 23:48:17 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string symbol;
+
+            Console.Write("Введите свое имя : ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите любой символ : ");
+            symbol = Console.ReadLine();
+
+            Console.WriteLine("\n" + name);
+            Console.WriteLine("\n" + symbol);
+            Console.WriteLine("");
+            for (int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+            
+            Console.WriteLine($"\n{symbol}{name}{symbol}");
+
+            for (int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.ReadKey();
+        }
+    }
+}
